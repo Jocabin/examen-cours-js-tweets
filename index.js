77fa1202e4063544b7886bd003fe18fada47e48b
Jocabin <kobyfar@hotmail.com>
2023-11-30 13:46:14 +0100

    Cleanup et ajout de commentaire

--- index.js
+++ index.js
@@ -32,21 +32,23 @@ const tweets = [
 ]
 
 
-// Question 1
-
 // On récupère le premier tweet de la liste donnée
 const firstTweet = tweets[0]
+
 // On récupère la liste <ul> tweets-list
 const tweetsList = document.getElementById('tweets-list')
 
-// définition d'une fonction pour créer des elements, leurs ajouter des classes et du contenu textuel rapidement
+// Définition d'une fonction pour créer des elements, leurs ajouter des classes et du contenu textuel rapidement
 function createElement(tagName, classList = [], content = '') {
+    // Création de l'élément HTML
     const element = document.createElement(tagName)
 
+    // Ajout de toutes ces classes
     classList.forEach(className => {
         element.classList.add(className)
     })
 
+    // Ajout de son contenu, uniquement si l'appel de la fonction le spécifie
     if (content) {
         element.textContent = content
     }
@@ -54,23 +56,30 @@ function createElement(tagName, classList = [], content = '') {
     return element
 }
 
+// Définition d'une fonction pour ajouter plusieurs élément HTML d'un seul coup à un autre élément du DOM
+function addElementsInBatch(parentElement, ...childsElements) {
+    childsElements.forEach(child => {
+        parentElement.appendChild(child)
+    })
+}
+
 tweets.forEach(tweet => {
-    // On créer l'élément li, ainsi que son contenu, nombre de likes et auteur
+    // On créer un tableau qui contient toutes les classes de la balise <li>
     const tweetClasses = ["tweet"]
 
+    // On ajoute la classe favorite uniquement si le nombre de likes est supérieur a 10
     if (tweet.likes > 10) {
         tweetClasses.push('favorite')
     }
 
+    // On créer l'élément li, ainsi que son contenu, nombre de likes et auteur
     const tweetWrapper = createElement('li', tweetClasses)
     const tweetContent = createElement('p', ['content'], tweet.content)
     const tweetLikes = createElement('p', ['likes'], tweet.likes)
     const tweetAuthor = createElement('p', ['author'], tweet.author)
 
     // On ajoute tous les éléments précedents à la balise <li>
-    tweetWrapper.appendChild(tweetContent)
-    tweetWrapper.appendChild(tweetLikes)
-    tweetWrapper.appendChild(tweetAuthor)
+    addElementsInBatch(tweetWrapper, tweetContent, tweetLikes, tweetAuthor)
 
     // On ajoute la balise <li> à la balise <ul>
     tweetsList.appendChild(tweetWrapper)
